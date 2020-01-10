f29bb3b7a31eebab76697205f37d959bcf46d353
Patrick Kennedy <pkennedy2@SF-US05643-MBP13.local>
2020-01-09 16:39:04 -0800

    added a the delete for the cohorts

--- queries.js
+++ queries.js
@@ -36,7 +36,7 @@ const createCohort = (request, response) => {
         if (error) {
             throw error
         }
-        response.status(201).send(`Cohort added with ID: ${result.insertId}`)
+        response.status(201).send(`Cohort added with ID: ${results.insertId}`)
     })
 }
 
